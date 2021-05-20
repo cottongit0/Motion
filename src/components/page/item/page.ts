0b7deb4cba1c04a06ef40a0054baa6cd60e89f1a
cottongit0 <cottongit0@gmail.com>
2021-05-20 20:21:41 +0900

    base component - 상속

--- src/components/page/item/page.ts
+++ src/components/page/item/page.ts
@@ -1,11 +1,7 @@
-export class PageComponent {
-  private element: HTMLUListElement;
+import { BaseCompoment } from "./../../component";
+
+export class PageComponent extends BaseCompoment<HTMLUListElement> {
   constructor() {
-    this.element = document.createElement("ul");
-    this.element.setAttribute("class", "page");
-    this.element.textContent = "페이지 컴포넌트입니다.";
-  }
-  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
-    parent.insertAdjacentElement(position, this.element);
+    super('<ul class="page">페이지 컴포넌트입니다.</ul>');
   }
 }
