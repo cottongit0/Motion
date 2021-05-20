24affb43644fe7f40b4acae033d607afa5abd3db
cottongit0 <cottongit0@gmail.com>
2021-05-20 23:14:22 +0900

    component - video

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
+import { VideoComponent } from "./components/page/item/video";
 import { ImageComponent } from "./components/page/item/image";
 import { PageComponent } from "./components/page/item/page";
 class App {
   private readonly page: PageComponent;
-  private readonly image: ImageComponent;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
 
-    this.image = new ImageComponent(
+    const image = new ImageComponent(
       "Image Title",
       "https://i.picsum.photos/id/902/536/354.jpg?hmac=4WH85q7uEdOeEmbcRc7lkRgcUFZimMZtYXqFroCGh0Y"
     );
-    this.image.attachTo(appRoot, "beforeend");
+    image.attachTo(appRoot, "beforeend");
+
+    const video = new VideoComponent(
+      "Video Title",
+      "https://youtu.be/K3-jG52XwuQ"
+    );
+    video.attachTo(appRoot, "beforeend");
   }
 }
 
