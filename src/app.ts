ccd953a6b60eb3a93304bd7444a577b1f4673c4d
cottongit0 <cottongit0@gmail.com>
2021-05-21 20:35:59 +0900

    Component - pageItem

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
+import { Component } from "./components/component";
 import { TodoComponent } from "./components/page/item/todo";
 import { NoteComponent } from "./components/page/item/note";
 import { VideoComponent } from "./components/page/item/video";
 import { ImageComponent } from "./components/page/item/image";
-import { PageComponent } from "./components/page/item/page";
+import { Composable, PageComponent } from "./components/page/item/page";
+
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
@@ -13,19 +15,19 @@ class App {
       "Image Title",
       "https://i.picsum.photos/id/902/536/354.jpg?hmac=4WH85q7uEdOeEmbcRc7lkRgcUFZimMZtYXqFroCGh0Y"
     );
-    image.attachTo(appRoot, "beforeend");
+    this.page.addChild(image);
 
     const note = new NoteComponent("Note Title", "내용을 작성해주세요.");
-    note.attachTo(appRoot, "beforeend");
+    this.page.addChild(note);
 
     const todo = new TodoComponent("Todo Title", "오늘의 할 일을 입력해주세요");
-    todo.attachTo(appRoot, "beforeend");
+    this.page.addChild(todo);
 
     const video = new VideoComponent(
       "Video Title",
       "https://youtu.be/K3-jG52XwuQ"
     );
-    video.attachTo(appRoot, "beforeend");
+    this.page.addChild(video);
   }
 }
 
--- src/components/page/item/page.ts
+++ src/components/page/item/page.ts
@@ -1,7 +1,39 @@
-import { BaseCompoment } from "./../../component";
+import { BaseCompoment, Component } from "./../../component";
 
-export class PageComponent extends BaseCompoment<HTMLUListElement> {
+export interface Composable {
+  addChild(child: Component): void;
+}
+
+class PageItemComponent
+  extends BaseCompoment<HTMLElement>
+  implements Composable
+{
   constructor() {
-    super('<ul class="page">페이지 컴포넌트입니다.</ul>');
+    super(`<li class="page-item">
+            <section class="page-item__body"></section>
+            <div class="page-item_-controls">
+              <button class="close">&times;</button>
+            </div>
+          </li>`);
+  }
+  addChild(child: Component) {
+    const container = this.element.querySelector(
+      ".page-item__body"
+    )! as HTMLElement;
+    child.attachTo(container);
+  }
+}
+
+export class PageComponent
+  extends BaseCompoment<HTMLUListElement>
+  implements Composable
+{
+  constructor() {
+    super('<ul class="page"></ul>');
+  }
+  addChild(section: Component) {
+    const item = new PageItemComponent();
+    item.addChild(section);
+    item.attachTo(this.element, "beforeend");
   }
 }
