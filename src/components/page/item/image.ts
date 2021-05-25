a0339313435f83dc88eb9360efc29328f195c4f3
cottongit0 <cottongit0@gmail.com>
2021-05-25 13:23:31 +0900

    오타 수정 - 클래스이름

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -4,13 +4,13 @@ export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
     super(`<section class="image">
             <div class="image__holder">
-              <img class="image__thumnail">
-              <h2 class="page-item__title image__title"></h2>
+              <img class="image__thumbnail">
             </div>
+            <h2 class="page-item__title image__title"></h2>
           </section>`);
 
     const imageElement = this.element.querySelector(
-      ".image__thumnail"
+      ".image__thumbnail"
     )! as HTMLImageElement;
     imageElement.src = url;
     imageElement.alt = title;
